48801cb473bc55dd5c4babb0f826d74ddcec589f
Maxime Vidori <maxime.vidori@gmail.com>
2014-08-16 19:26:14 +0200

    Add tests for listFiles

--- test/main.js
+++ test/main.js
@@ -1,10 +1,32 @@
-var assert = require('assert');
+var assert            = require('assert');
+var _                 = require('lodash');
 var angularDependency = require('../');
 
 
 describe('angular-dependency', function () {
 
-  it('should return an hello function', function () {
-  	assert.equal(angularDependency.hello(), 'Hello World!');		
-  });
+	describe('listFiles', function () {
+		it('should list all files in the sample directory', function () {
+			var sampleFiles = [
+			'test/sample/content/content.js',
+			'test/sample/content/dependency3content/dependency3content.js',
+		  'test/sample/dependencies/dependency1.js',
+		  'test/sample/dependencies/dependency2/content/dependency2content.js',
+		  'test/sample/dependencies/dependency2/dependency2.js',
+		  'test/sample/dependencies/dependency3/dependency3.js',
+		  'test/sample/dependencies/dependency5.js',
+		  'test/sample/dependencies/foo.js',
+		  'test/sample/main.js' ];
+
+			var files = [];
+			angularDependency.listFiles('test/sample', files);
+
+			var t  = _(files)
+			.zip(sampleFiles)
+			.each(function (files) {
+				assert.equal(files[0], files[1]);
+			});
+			
+		});
+	});
 });
